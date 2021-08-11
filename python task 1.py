c8819ce74e3419506b002f16cc96543b011dc804
Ritikakri935 <78211472+Ritikakri935@users.noreply.github.com>
2021-08-11 11:51:07 +0530

    Add files via upload

---
+++ python task 1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+r=float(input("inter readius"))
+pi=22/7
+print("area=",pi*(r**2))
+
+
+# In[2]:
+
+
+r=float(input("inter readius"))
+pi=22/7
+area=pi*(r**2)
+print(area)
+
+
+# In[3]:
+
+
+r=float(input("inter readius"))
+pi=3.14
+area=pi*(r**2)
+print(area)
+
+
+# In[ ]:
+
+
+
+
